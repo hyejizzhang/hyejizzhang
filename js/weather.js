306253a2496cb7c6e77910854439b3ae7c1d8ec6
hyejizzhang <119471011+hyejizzhang@users.noreply.github.com>
2022-11-30 12:39:58 +0900

    Add files via upload

---
+++ js/weather.js
@@ -0,0 +1,38 @@
+$(function(){
+
+    $(window).on('load', function(){
+        $('.wrapper').css({
+            'background-image': 'url(./images/1.jpg)'
+        });
+    });
+    $('a.btn').click(function(e){
+        e.preventDefault();
+        let w = $('.searchbox').css('width');
+        if(w != '350px'){
+        $('.searchbox').css({
+            opacity: 1,
+            width: '350px'
+        });
+      }else{
+        $('.searchbox').css({
+            opacity: 0,
+            width: '0px'
+        });
+      }
+    });
+
+    daySlide();
+});
+
+function daySlide(){
+    $('.five-day').slick({
+        centerMode: true,
+        centerPadding: '20px',
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 2000
+    });
+}
